cc3fc5db9e4f80f5059767cb675871be46ee2e63
Oleksii Shevchenko <a380987379715@gmail.com>
2023-03-03 22:57:27 +0100

    creating bot v3

---
+++ cmd/internal/config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+type Config struct {
+	Telegram
+	AbstractAPI
+}
+
+type Telegram struct {
+	TokenENV    string `split_words:"true"`
+	APIBaseURL  string `split_words:"true"`
+	SendMessage string `split_words:"true"`
+}
+
+type AbstractAPI struct {
+	AbstractApiBaseUrl  string `envconfig:"ABSTRACT_API_BASE_URL"`
+	AbstractApiTokenEnv string `envconfig:"ABSTRACT_API_TOKEN_ENV"`
+}
+
+func ReadConfigTG(pathToEnv string) (*Telegram, error) {
+	if err := godotenv.Load(pathToEnv); err != nil {
+		return nil, fmt.Errorf("error parsing config: %w", err)
+	}
+	c := &Telegram{}
+
+	if err := envconfig.Process("TELEGRAM_API", c); err != nil {
+		return nil, fmt.Errorf("error process config: %w", err)
+	}
+
+	return c, nil
+}
+
+func ReadConfigAbstractAPI(pathToEnv string) (*AbstractAPI, error) {
+	if err := godotenv.Load(pathToEnv); err != nil {
+		return nil, fmt.Errorf("error parsing config: %v", err)
+	}
+	c := &AbstractAPI{}
+
+	if err := envconfig.Process("", c); err != nil {
+		return nil, fmt.Errorf("error process config: %w", err)
+	}
+	return c, nil
+}
+
+func Get() (*Config, error) {
+	CfgTG, err := ReadConfigTG("env/telegramBot.env")
+	if err != nil {
+		log.Fatalf("can't read config from file, error: %w", err)
+		return nil, err
+	}
+	CfgAbstract, err := ReadConfigAbstractAPI("/home/shevchenko/GolandProjects/education/task2.3.3/env/abstractAPI.env")
+	if err != nil {
+		log.Fatalf("can't read config from file, error: %v", err)
+		return nil, err
+	}
+	cfg := &Config{
+		Telegram:    *CfgTG,
+		AbstractAPI: *CfgAbstract,
+	}
+	return cfg, nil
+
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module task2.3.3
+
+go 1.19
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
